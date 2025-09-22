0c797250849d616f67652a36db312cbabfc9de8c
dr-spider-boy <jackwatson59033@gmail.com>
2025-09-22 17:48:06 -0600

    Prep 3 challange complete.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the game. There's no turnign back now.");
+        Console.WriteLine("The magic number is bewteen 1 and 100 including.");
+        Random randomgenerator = new Random();
+        int magicnumber = randomgenerator.Next(1, 100);
+        string guess;
+        int intguess;
+        int tracker = 0;
+        do
+        {
+            Console.Write("What is your guess?");
+            guess = Console.ReadLine();
+            intguess = int.Parse(guess);
+            tracker++;
+            if (intguess == magicnumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (intguess < magicnumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (intguess > magicnumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        } while (intguess != magicnumber);
+        Console.WriteLine($"It took you {tracker} guesses!");
     }
 }
