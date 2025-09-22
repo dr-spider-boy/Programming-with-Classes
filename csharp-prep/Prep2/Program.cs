652f629b4d035cd4c8741322a3c77861a0c23dcc
dr-spider-boy <jackwatson59033@gmail.com>
2025-09-22 08:45:29 -0600

    Week one final modifications.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade?");
+        string userinput = Console.ReadLine();
+        int grade = int.Parse(userinput);
+        string lettergrade;
+        if (grade >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            lettergrade = "d";
+        }
+        else
+        {
+            lettergrade = "F";
+        }
+        Console.WriteLine($"Your grade is {grade}.");
+        Console.WriteLine($"Your earned a {lettergrade} in the course.");
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!!");
+        }
+        else
+        {
+            Console.WriteLine("You have failed. Don't try again.");
+        }
     }
 }
