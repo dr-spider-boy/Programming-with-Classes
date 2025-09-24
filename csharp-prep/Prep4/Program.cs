525632fe3708421617f1ec975c761c52952ab2de
dr-spider-boy <jackwatson59033@gmail.com>
2025-09-23 18:33:25 -0600

    This is the end of project 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int sum = 0;
+        float average;
+        int maximum = 0;
+
+        int number;
+        List<int> numbers;
+        numbers = new List<int>();
+        Console.WriteLine("Write down some numbers to add to a list. Type 0 when finished.");
+        do
+        {
+            Console.Write("Enter Number:");
+            string userinput = Console.ReadLine();
+            number = int.Parse(userinput);
+            numbers.Add(number);
+            if (number > maximum)
+            {
+                maximum = number;
+            }
+        } while (number != 0);
+        foreach (int item in numbers)
+        {
+            sum = sum + item;
+        }
+        average = sum / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maximum}");
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,66 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        welcome();
+        int birthyear;
+        BirthYear(out birthyear);
+
+        display(birthyear);
+    }
+
+
+
+
+
+    static void welcome()
+    {
+        Console.WriteLine("welcome to the program!");
+    }
+
+    static string promptusername()
+    {
+        Console.Write("What is your name?");
+        string username = Console.ReadLine();
+        return username;
+    }
+
+    static int favoritenumber()
+    {
+        Console.Write("What is your favorite number?");
+        string usernumber = Console.ReadLine();
+        int number = int.Parse(usernumber);
+        return number;
+    }
+
+    static void BirthYear(out int birthyear)
+    {
+        Console.Write("What year were you born?");
+        string userinput = Console.ReadLine();
+        birthyear = int.Parse(userinput);
+    }
+
+    static int squarenumber()
+    {
+        Console.Write("Give me a random number?");
+        string randominput = Console.ReadLine();
+        int randomnumber = int.Parse(randominput);
+        int squarednumber = randomnumber * randomnumber;
+        return squarednumber;
+    }
+
+    static void display(int birthyear)
+    {
+        string username = promptusername();
+        int number = favoritenumber();
+        int squarednumber = squarenumber();
+        Console.WriteLine($"Hello {username}, your favorite number is: {number}.");
+        Console.WriteLine($"The square of your random number is: {squarednumber}.");
+        Console.WriteLine($"You are {2025 - birthyear} years old.");
     }
 }
