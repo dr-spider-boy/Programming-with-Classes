ff792fcac22d4e3bbfa40da3b526f0229d6e4b85
dr-spider-boy <jackwatson59033@gmail.com>
2025-10-15 20:07:11 -0600

    this is the learning module for the past.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobtitle = "Electrical Engineer";
+        job1._company = "Lockheed Martin";
+        job1._startyear = 2028;
+        job1._endyear = 2038;
+
+
+        Job job2 = new Job();
+        job2._jobtitle = "Unemployed";
+        job2._company = "None";
+        job2._startyear = 2025;
+        job2._endyear = 2026;
+
+        Resume myresume = new Resume();
+        myresume._name = "Jack Watson";
+
+        myresume._listofjobs.Add(job1);
+        myresume._listofjobs.Add(job2);
+
+        myresume.Display();
     }
 }
