0df8c88f1f0d3a818933a929eb0d64df06e6ac4a
dr-spider-boy <jackwatson59033@gmail.com>
2025-11-03 10:08:16 -0700

    learning 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Samuel Bennett", "Muliplication");
+        Console.WriteLine(a1.GetSummary());
+
+        Math a2 = new Math("Roberto Rodriguez", "Fractions", "7.3", "8-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+
+        Writing a3 = new Writing("Mary Waters", "Eurpean history", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
